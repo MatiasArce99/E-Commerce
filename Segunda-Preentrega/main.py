44284469a2724cc3f24c9cc3fdf820413460f012
Matías <matiasarce214@gmail.com>
2024-06-21 19:07:54 -0300

    Incluyendo módulo 2

--- Segunda-Preentrega/main.py
+++ Segunda-Preentrega/main.py
@@ -1,5 +1,5 @@
 from paquete1.modulo1 import Cliente
-#from paquete1.modulo2 import *
+from paquete1.modulo2 import *
 
 cliente1 = Cliente('Matías Arce', 34603291, 'Teclado mecánico', 111222)
 cliente1.detalleCompra()
