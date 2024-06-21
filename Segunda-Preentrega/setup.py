9e37c4d4c4387128503f00b355e68dcc34635261
Matías <matiasarce214@gmail.com>
2024-06-21 19:03:35 -0300

    Creando paquetes redistribuibles

---
+++ Segunda-Preentrega/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    
+    name='Segunda entrega',
+    version = '1.0',
+    description = 'Paquete redistribuible',
+    author = 'Matías Arce',
+    packages = ['paquete1']
+    
+)
